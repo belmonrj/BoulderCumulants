f356b23dbca734019f12b688f6f78e0c2413ff1b
Ron Belmont <belmonrj@gmail.com>
2016-12-13 00:29:32 -0500

    Minor updates to run macro

--- Run_BoulderCumulants.C
+++ Run_BoulderCumulants.C
@@ -25,9 +25,8 @@ void Run_BoulderCumulants(const char *outFile = "test_train_output.root")
 
   BoulderCumulants *sflow = new BoulderCumulants();
   sflow->set_use_runlist(false);
-  sflow->set_do_offsets(false);
   sflow->set_output_filename(outFile);
-  sflow->Verbosity(1);
+  sflow->Verbosity(0);
   se->registerSubsystem(sflow);
 
 }
