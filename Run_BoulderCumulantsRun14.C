5aac0f3a593e2e2738530a97ab3811d780a087a3
Ron Belmont <belmonrj@gmail.com>
2016-12-22 21:32:09 -0500

    Added macro specifically for Run14

---
+++ Run_BoulderCumulantsRun14.C
@@ -0,0 +1,42 @@
+void Run_BoulderCumulantsRun14(const char *outFile = "test_train_output.root")
+{
+
+  //-----------------//
+  //--- Libraries ---//
+  //-----------------//
+
+  //gSystem->Load("libfvtx_subsysreco.so"); // must have for Run15 and must remove for Run16 and Run14
+  gSystem->Load("libdAuBES_utils.so");
+  gSystem->Load("libBoulderCumulants.so");
+  gSystem->ListLibraries();
+
+  //----------------------//
+  //--- Fun4All Server ---//
+  //----------------------//
+
+  Fun4AllServer *se = Fun4AllServer::instance();
+
+  // To get the FVTX tracks
+  se->registerSubsystem(new FvtxReadbackDST());
+
+  //--------------------//
+  //--- User Module ----//
+  //--------------------//
+
+  BoulderCumulants *sflow = new BoulderCumulants();
+  sflow->set_use_runlist(false);
+  sflow->set_output_filename(outFile);
+  sflow->Verbosity(0);
+  se->registerSubsystem(sflow);
+
+}
+
+void InputData(vector<string> &indata)
+{
+  // indata.push_back("CNT");
+  // indata.push_back("DST_EVE"); // must have for Run15 and Run16
+  // indata.push_back("DST_FVTX"); // must have for Run15 and must remove for Run16
+  indata.push_back("MWG"); // must have for Run14 (only)
+  indata.push_back("MuonDST"); // must have for Run14 (only)
+  return;
+}
