883f4008900bd9bef5fbb74805bd980cd8c7e48a
Ron Belmont <belmonrj@gmail.com>
2017-01-09 22:10:24 -0500

    Added clean function to de-weight empty bins

--- work/do_systematics.C
+++ work/do_systematics.C
@@ -1,3 +1,7 @@
+void process(const char*, const char*);
+double get_mean(const TH1D*);
+void clean(TH1D*);
+
 void do_systematics()
 {
 
@@ -28,7 +32,6 @@ void process(const char* systematic, const char* collision)
   TH1D* v22_base    = (TH1D*)finbase->Get(Form("th1dR1_v22_%s",collision));
   TH1D* v24_base    = (TH1D*)finbase->Get(Form("th1dR1_v24_%s",collision));
 
-
   TFile* finsyst = TFile::Open(Form("all_cumulants_%s.root",systematic));
   TH1D* v22gap_syst = (TH1D*)finsyst->Get(Form("th1dR1_v22gap_%s",collision));
   TH1D* v22_syst    = (TH1D*)finsyst->Get(Form("th1dR1_v22_%s",collision));
@@ -41,6 +44,7 @@ void process(const char* systematic, const char* collision)
   tex->SetTextSize(0.05);
 
   v22gap_syst->Divide(v22gap_base);
+  clean(v22gap_syst);
   v22gap_syst->SetMarkerStyle(kFullCircle);
   v22gap_syst->SetMarkerColor(kBlack);
   v22gap_syst->SetLineColor(kBlack);
@@ -49,14 +53,17 @@ void process(const char* systematic, const char* collision)
   v22gap_syst->GetXaxis()->SetRangeUser(0,70);
   v22gap_syst->SetMaximum(2.0);
   v22gap_syst->SetMinimum(0.0);
-  fun->SetParameter(0,1);
-  v22gap_syst->Fit(fun,"QWR");
+  //fun->SetParameter(0,1);
+  //fun->FixParameter(0,get_mean(v22gap_syst));
+  v22gap_syst->Fit(fun,"R");
+  //fun->Draw("same");
   num = fun->GetParameter(0);
   tex->DrawLatex(0.2,0.8,Form("%s systematic, %.2f%%", systematic, fabs((num-1)*100) ));
   c1->Print(Form("SysFigsFour/sys_v22gap_%s_%s.png",systematic,collision));
   c1->Print(Form("SysFigsFour/sys_v22gap_%s_%s.pdf",systematic,collision));
 
   v22_syst->Divide(v22_base);
+  clean(v22_syst);
   v22_syst->SetMarkerStyle(kFullCircle);
   v22_syst->SetMarkerColor(kBlack);
   v22_syst->SetLineColor(kBlack);
@@ -65,14 +72,17 @@ void process(const char* systematic, const char* collision)
   v22_syst->GetXaxis()->SetRangeUser(0,70);
   v22_syst->SetMaximum(2.0);
   v22_syst->SetMinimum(0.0);
-  fun->SetParameter(0,1);
-  v22_syst->Fit(fun,"QWR");
+  //fun->SetParameter(0,1);
+  //fun->FixParameter(0,get_mean(v22_syst));
+  v22_syst->Fit(fun,"R");
   num = fun->GetParameter(0);
+  //fun->Draw("same");
   tex->DrawLatex(0.2,0.8,Form("%s systematic, %.2f%%", systematic, fabs((num-1)*100) ));
   c1->Print(Form("SysFigsFour/sys_v22_%s_%s.png",systematic,collision));
   c1->Print(Form("SysFigsFour/sys_v22_%s_%s.pdf",systematic,collision));
 
   v24_syst->Divide(v24_base);
+  clean(v24_syst);
   v24_syst->SetMarkerStyle(kFullCircle);
   v24_syst->SetMarkerColor(kBlack);
   v24_syst->SetLineColor(kBlack);
@@ -81,8 +91,10 @@ void process(const char* systematic, const char* collision)
   v24_syst->GetXaxis()->SetRangeUser(0,70);
   v24_syst->SetMaximum(2.0);
   v24_syst->SetMinimum(0.0);
-  fun->SetParameter(0,1);
+  //fun->SetParameter(0,1);
+  //fun->FixParameter(0,get_mean(v24_syst));
   v24_syst->Fit(fun,"","",10,60);
+  //fun->Draw("same");
   num = fun->GetParameter(0);
   tex->DrawLatex(0.2,0.8,Form("%s systematic, %.2f%%", systematic, fabs((num-1)*100) ));
   c1->Print(Form("SysFigsFour/sys_v24_%s_%s.png",systematic,collision));
@@ -90,3 +102,26 @@ void process(const char* systematic, const char* collision)
 
 }
 
+double get_mean(const TH1D* h)
+{
+  int nbins = h->GetNbinsX();
+  double total = 0;
+  for ( int i = 0; i < nbins; ++i )
+    {
+      double content = h->GetBinContent(i+1);
+      if ( content > 0 ) total += content;
+    }
+  double final = total/nbins;
+  cout << final << endl;
+  return final;
+}
+
+void clean(TH1D* h)
+{
+  int nbins = h->GetNbinsX();
+  for ( int i = 0; i < nbins; ++i )
+    {
+      double content = h->GetBinContent(i+1);
+      if ( content < 0 ) h->SetBinError(i+1,999);
+    }
+}
