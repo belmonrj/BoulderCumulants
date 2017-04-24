b1c97c6e2c0233157303d3637e9b6a49dddeaabe
Ron Belmont <belmonrj@gmail.com>
2017-04-24 17:34:30 -0400

    Changed output figure directory and added analogous macro for nfvtxt stuff

--- work/helpmestrk.C
+++ work/helpmestrk.C
@@ -1,15 +1,15 @@
-void helpmecent()
+void helpmestrk()
 {
 
   TCanvas* c1 = new TCanvas();
 
-  TFile* fin = TFile::Open("all_cumulants_cent.root");
-  TH1D* th1d_v22gap_base = (TH1D*)fin->Get("th1dR1_v22gap_Run14AuAu200");
-  TH1D* th1d_v22_base = (TH1D*)fin->Get("th1dR1_v22_Run14AuAu200");
-  TH1D* th1d_v24_base = (TH1D*)fin->Get("th1dR1_v24_Run14AuAu200");
-  TH1D* th1d_v22gap_unco = (TH1D*)fin->Get("th1dR1U_v22gap_Run14AuAu200");
-  TH1D* th1d_v22_unco = (TH1D*)fin->Get("th1dR1U_v22_Run14AuAu200");
-  TH1D* th1d_v24_unco = (TH1D*)fin->Get("th1dR1U_v24_Run14AuAu200");
+  TFile* fin = TFile::Open("all_cumulants_strk.root");
+  TH1D* th1d_v22gap_base = (TH1D*)fin->Get("th1dR10_v22gap_Run14AuAu200");
+  TH1D* th1d_v22_base = (TH1D*)fin->Get("th1dR10_v22_Run14AuAu200");
+  TH1D* th1d_v24_base = (TH1D*)fin->Get("th1dR10_v24_Run14AuAu200");
+  TH1D* th1d_v22gap_unco = (TH1D*)fin->Get("th1dR10U_v22gap_Run14AuAu200");
+  TH1D* th1d_v22_unco = (TH1D*)fin->Get("th1dR10U_v22_Run14AuAu200");
+  TH1D* th1d_v24_unco = (TH1D*)fin->Get("th1dR10U_v24_Run14AuAu200");
 
   th1d_v22gap_base->SetMarkerStyle(kFullDiamond);
   th1d_v22_base->SetMarkerStyle(kFullCircle);
@@ -30,7 +30,7 @@ void helpmecent()
   th1d_v22_base->Draw("ex0p");
   th1d_v22_base->SetMinimum(0.0);
   th1d_v22_base->SetMaximum(0.199);
-  th1d_v22_base->GetXaxis()->SetTitle("centrality (%)");
+  th1d_v22_base->GetXaxis()->SetTitle("N^{FVTX}_{tracks}");
   th1d_v22_base->GetYaxis()->SetTitle("v_{2}");
   th1d_v24_base->Draw("ex0p same");
   th1d_v22gap_base->Draw("ex0p same");
@@ -47,8 +47,8 @@ void helpmecent()
   leg->AddEntry(th1d_v22gap_base,"v_{2}{2,|#Delta#eta|>2}","p");
   leg->Draw();
 
-  c1->Print("FigsFourSpecial/acceptance_AuAu.png");
-  c1->Print("FigsFourSpecial/acceptance_AuAu.pdf");
+  c1->Print("FigsFourSpecial/strk_acceptance_AuAu.png");
+  c1->Print("FigsFourSpecial/strk_acceptance_AuAu.pdf");
 
   TH1D* th1d_cos1 = (TH1D*)fin->Get("th1d_cos1");
   TH1D* th1d_sin1 = (TH1D*)fin->Get("th1d_sin1");
@@ -72,8 +72,8 @@ void helpmecent()
   th1d_cos1->Draw("ex0p same");
   th1d_sin1->Draw("ex0p same");
 
-  c1->Print("FigsFourSpecial/acceptance_AuAu_v22andterms.png");
-  c1->Print("FigsFourSpecial/acceptance_AuAu_v22andterms.pdf");
+  c1->Print("FigsFourSpecial/strk_acceptance_AuAu_v22andterms.png");
+  c1->Print("FigsFourSpecial/strk_acceptance_AuAu_v22andterms.pdf");
 
   th1d_cos1_north->SetLineColor(kBlack);
   th1d_cos1_north->SetMarkerColor(kBlack);
@@ -97,8 +97,8 @@ void helpmecent()
   th1d_sin1_north->Draw("ex0p same");
   th1d_sin1_south->Draw("ex0p same");
 
-  c1->Print("FigsFourSpecial/acceptance_AuAu_v22gapandterms.png");
-  c1->Print("FigsFourSpecial/acceptance_AuAu_v22gapandterms.pdf");
+  c1->Print("FigsFourSpecial/strk_acceptance_AuAu_v22gapandterms.png");
+  c1->Print("FigsFourSpecial/strk_acceptance_AuAu_v22gapandterms.pdf");
 
   // TLegend* leg = new TLegend(0.68,0.58,0.88,0.88);
   // leg->AddEntry(th1d_c22gap_base);
@@ -108,8 +108,8 @@ void helpmecent()
   th1d_c22gap_base->Multiply(th1d_v22gap_base);
   th1d_c22gap_unco->Multiply(th1d_v22gap_unco);
 
-  c1->Print("FigsFourSpecial/acceptance_AuAu_c22gapandterms.png");
-  c1->Print("FigsFourSpecial/acceptance_AuAu_c22gapandterms.pdf");
+  c1->Print("FigsFourSpecial/strk_acceptance_AuAu_c22gapandterms.png");
+  c1->Print("FigsFourSpecial/strk_acceptance_AuAu_c22gapandterms.pdf");
 
 
 
