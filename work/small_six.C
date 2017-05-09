b27419ac6561b16e44544c4b913a808561ffbfe6
Ron Belmont <belmonrj@gmail.com>
2017-05-09 01:14:31 -0400

    Short term fix for the error bars, added 2-panel components plot, added sys errors

--- work/small_six.C
+++ work/small_six.C
@@ -62,23 +62,26 @@ void doit(const char* handle, int rebin)
   for ( int i = 0; i < nbins; ++i )
     {
       // --- 6-particle
+      double center = th1d_c26->GetBinCenter(i+1);
+      if ( center < 7 ) th1d_c26->SetBinContent(i+1,-9999);
       double c26 = th1d_c26->GetBinContent(i+1);
       double v26 = -9999;
       double six = th1d_six->GetBinContent(i+1);
       double esix = th1d_six->GetBinError(i+1);
-      double ev26 = v26*(esix/six); // relative error for now, will get proper formula later...
-      ev26 = 0; // looks very bad for reasons I don't yet understand...
       if ( c26 > 0 && six != 0 ) v26 = pow((0.25*c26),(1.0/6.0)); // v2{6} = (c2{6}/4)^{(1/6)}
       th1d_v26->SetBinContent(i+1,v26);
+      double ev26 = v26*(esix/six); // relative error for now, will get proper formula later...
+      //if ( six > 0 ) cout << v26 << " " << (esix/six) << " " << ev26 << endl;
+      //ev26 = 0; // looks very bad for reasons I don't yet understand...
       th1d_v26->SetBinError(i+1,ev26);
       // --- 4-particle
       double c24 = th1d_c24->GetBinContent(i+1);
       double v24 = -9999;
       double four = th1d_for->GetBinContent(i+1);
       double efour = th1d_for->GetBinError(i+1);
-      double ev24 = v24*(efour/four); // relative error for now, will get proper formula later...
-      ev24 = 0; // looks very bad for reasons I don't yet understand...
       if ( c24 < 0 && four != 0 ) v24 = pow(-c24,(1.0/4.0)); // v2{4} = -c2{4}^{(1/4)}
+      double ev24 = v24*(efour/four); // relative error for now, will get proper formula later...
+      //ev24 = 0; // looks very bad for reasons I don't yet understand...
       th1d_v24->SetBinContent(i+1,v24);
       th1d_v24->SetBinError(i+1,ev24);
       // --- 2-particle
@@ -96,11 +99,38 @@ void doit(const char* handle, int rebin)
       // cout << i << " six " << th1d_six->GetBinContent(i+1) << " " << six << endl;
     }
 
+
+  float Lmarg = 0.14;
+  float Rmarg = 0.02;
+  // float Bmarg = 0.10;
+  // float Bmarg = 0.14; // not quite enough
+  //float Bmarg = 0.2; // a little too much
+  float Bmarg = 0.16;
+  float Tmarg = 0.02;
+
+  TCanvas *ccomp_dAu200 = new TCanvas("ccomp_dAu200", "components dau200", 600, 700);
+  ccomp_dAu200->SetMargin(0, 0, 0, 0);
+
+  ccomp_dAu200->cd();
+  TPad *pcomp = new TPad("pcomp", "comp", 0, 0.45, 1, 1);
+  pcomp->SetMargin(Lmarg, Rmarg, 0, 0.1);
+  pcomp->SetTicks(1, 1);
+  pcomp->Draw();
+
+  ccomp_dAu200->cd();
+  TPad *pcompc26 = new TPad("pcompc24", "compc24", 0, 0, 1, 0.45);
+  pcompc26->SetMargin(Lmarg, Rmarg, 0.22, 0);
+  pcompc26->SetTicks(1, 1);
+  pcompc26->Draw();
+
   double xmin = 0.0;
   double xmax = 50.0;
-  double ymin = -1e-4;
-  double ymax = 1e-4;
+  // double ymin = -0.999e-5;
+  // double ymax = 3e-5;
+  double ymin = -0.999e-5;
+  double ymax = 4e-5;
   TH2D* empty = new TH2D("empty","",1,xmin,xmax,1,ymin,ymax);
+  pcomp->cd();
   empty->Draw();
   //empty->GetXaxis()->SetTitle("N^{1<|#eta|<3}_{trk}");
   empty->GetXaxis()->SetTitle("N^{FVTX}_{tracks}");
@@ -126,9 +156,33 @@ void doit(const char* handle, int rebin)
   leg->AddEntry(th1d_942,"9#LT#LT4#GT#GT#LT#LT2#GT#GT","p");
   leg->AddEntry(th1d_123,"12#LT#LT2#GT#GT^{3}","p");
   leg->Draw();
-  c1->Print(Form("FigsSixSmall/strk_sixparticle_components_%s.png",handle));
-  c1->Print(Form("FigsSixSmall/strk_sixparticle_components_%s.pdf",handle));
+  pcompc26->cd();
+  ymax = 2e-6;
+  ymin = -5e-7;
+  TH2D* empty2 = new TH2D("empty2","",1,xmin,xmax,1,ymin,ymax);
+  empty2->Draw();
+  empty2->GetXaxis()->SetTitle("N^{FVTX}_{tracks}");
+  empty2->GetYaxis()->SetTitle("c_{2}{6}");
+  empty2->GetXaxis()->SetTitleSize(0.06);
+  empty2->GetYaxis()->SetTitleSize(0.06);
+  empty2->GetXaxis()->SetLabelSize(0.06);
+  empty2->GetYaxis()->SetLabelSize(0.06);
+  th1d_c26->SetMarkerStyle(kFullCircle);
+  th1d_c26->SetMarkerColor(kBlack);
+  th1d_c26->SetLineColor(kBlack);
+  th1d_c26->Draw("same ex0p");
+  TLine line(xmin,0,xmax,0);
+  line.SetLineWidth(2);
+  line.SetLineStyle(2);
+  line.Draw();
+  ccomp_dAu200->cd();
+  ccomp_dAu200->Print(Form("FigsSixSmall/strk_sixparticle_components_%s.png",handle));
+  ccomp_dAu200->Print(Form("FigsSixSmall/strk_sixparticle_components_%s.pdf",handle));
 
+  //return;
+
+  TCanvas* c1 = new TCanvas();
+  c1->cd();
   // xmin = 0.0;
   // xmax = 100.0;
   ymin = -2e-5;
@@ -205,6 +259,67 @@ void doit(const char* handle, int rebin)
   leg->AddEntry(th1d_v24,"v_{2}{4}","p");
   leg->AddEntry(th1d_v22,"v_{2}{2}","p");
   leg->Draw();
+  double sysB_v22_dAu200 = 0.1;
+  TH1D* gv22_sys_dAu200 = (TH1D*) th1d_v22->Clone("gv22_sys_dAu200");
+  gv22_sys_dAu200->SetMarkerSize(0);
+  gv22_sys_dAu200->SetFillColorAlpha(kBlue, 0.35);
+  for (int i = 1; i <= gv22_sys_dAu200->GetNbinsX(); i++)
+  {
+    double y = gv22_sys_dAu200->GetBinContent(i);
+    if ( y > 0 )
+    {
+      gv22_sys_dAu200->SetBinError(i, y * sysB_v22_dAu200);
+      //cout << y << " +- " << y * sysB_v22_dAu200 << endl;
+    }
+    else
+    {
+      gv22_sys_dAu200->SetBinContent(i, 0);
+      gv22_sys_dAu200->SetBinError(i, 0);
+    }
+  } // i
+  gv22_sys_dAu200->Draw("E5 same");
+  double sysB_v24_dAu200 = TMath::Sqrt(0.10 * 0.10 + // acceptance
+                                       0.10 * 0.10 + // zvrtx
+                                       0.01 * 0.01 + // DCA
+                                       0.01 * 0.01); // ghost tracks
+  //cout << "sys is " << sysB_v24_dAu200 << endl;
+  TH1D* gv24_sys_dAu200 = (TH1D*) th1d_v24->Clone("gv24_sys_dAu200");
+  gv24_sys_dAu200->SetMarkerSize(0);
+  gv24_sys_dAu200->SetFillColorAlpha(kRed, 0.35);
+  for (int i = 1; i <= gv24_sys_dAu200->GetNbinsX(); i++)
+  {
+    double y = gv24_sys_dAu200->GetBinContent(i);
+    if ( y > 0 )
+    {
+      gv24_sys_dAu200->SetBinError(i, y * sysB_v24_dAu200);
+      //cout << y << " +- " << y * sysB_v24_dAu200 << endl;
+    }
+    else
+    {
+      gv24_sys_dAu200->SetBinContent(i, 0);
+      gv24_sys_dAu200->SetBinError(i, 0);
+    }
+  } // i
+  gv24_sys_dAu200->Draw("E5 same");
+  double sysB_v26_dAu200 = sysB_v24_dAu200;
+  TH1D* gv26_sys_dAu200 = (TH1D*) th1d_v26->Clone("gv26_sys_dAu200");
+  gv26_sys_dAu200->SetMarkerSize(0);
+  gv26_sys_dAu200->SetFillColorAlpha(kBlack, 0.35);
+  for (int i = 1; i <= gv26_sys_dAu200->GetNbinsX(); i++)
+  {
+    double y = gv26_sys_dAu200->GetBinContent(i);
+    if ( y > 0 )
+    {
+      gv26_sys_dAu200->SetBinError(i, y * sysB_v26_dAu200);
+      //cout << y << " +- " << y * sysB_v26_dAu200 << endl;
+    }
+    else
+    {
+      gv26_sys_dAu200->SetBinContent(i, 0);
+      gv26_sys_dAu200->SetBinError(i, 0);
+    }
+  } // i
+  gv26_sys_dAu200->Draw("E5 same");
   c1->Print(Form("FigsSixSmall/strk_sixparticle_v2642_%s.png",handle));
   c1->Print(Form("FigsSixSmall/strk_sixparticle_v2642_%s.pdf",handle));
 
