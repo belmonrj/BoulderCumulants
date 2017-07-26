437072f010410f9ff96e0d1573303e05a2adf8ab
Ron Belmont <belmonrj@gmail.com>
2017-07-26 17:21:05 -0400

    Don't try this

---
+++ WorkAuAu/EventCutsRevised/donttrythis.C
@@ -0,0 +1,21 @@
+void donttrythis()
+{
+
+  TCanvas* c1 = new TCanvas("c1","");
+
+  TFile* file = TFile::Open("input/cumulants_Run14AuAu200.root");
+  TH2D* histo = (TH2D*)file->Get("th2d_nfvtxt_bbcsumratio");
+  TFile* file2 =  TFile::Open("jout.root");
+  TGraph* tg_upper = (TGraph*)file2->Get("tgupper");
+  TGraph* tg_lower = (TGraph*)file2->Get("tglower");
+
+  histo->RebinX(10);
+  histo->GetXaxis()->SetRangeUser(0,1000);
+  histo->Draw("colz");
+  tg_upper->Draw("l");
+  tg_lower->Draw("l");
+
+  c1->Print("noreallydonttrythis.png");
+
+
+}
