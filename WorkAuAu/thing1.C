2b330ba6104a3a80c26a18b8936563c8bc7db98c
Ron Belmont <belmonrj@gmail.com>
2017-08-08 23:01:12 -0400

    Added additional histos and comparisons

--- WorkAuAu/thing1.C
+++ WorkAuAu/thing1.C
@@ -1,5 +1,5 @@
 
-const int number_of_tests = 7; // update as needed
+const int number_of_tests = 10; // update as needed, but don't want to go beyond 10...
 const int xhash = 0;
 const int xdesc = 1;
 TString hash_description[2][number_of_tests];
@@ -16,18 +16,24 @@ void thing1()
   hash_description[0][4] = "11511";  hash_description[1][4] = "Jin 2";
   hash_description[0][5] = "11539";  hash_description[1][5] = "RemJ 1";
   hash_description[0][6] = "11540";  hash_description[1][6] = "RemJ 2";
+  hash_description[0][7] = "11552";  hash_description[1][7] = "Base";
+  hash_description[0][8] = "11596";  hash_description[1][8] = "DTC";
+  hash_description[0][9] = "11601";  hash_description[1][9] = "New Stuff";
 
   gROOT->ProcessLine(".L dothething_diagnostics.C");
   dothething_diagnostics(200,0,1,2,4);
   dothething_diagnostics(200,3,4,5,6);
+  dothething_diagnostics(200,4,7,8,9);
 
   gROOT->ProcessLine(".L dothething_components.C");
   dothething_components(200,0,1,2,4);
   dothething_components(200,3,4,5,6);
+  dothething_components(200,4,7,8,9);
 
   gROOT->ProcessLine(".L dothething_cumulants.C");
   dothething_cumulants(200,0,1,2,4);
   dothething_cumulants(200,3,4,5,6);
+  dothething_cumulants(200,4,7,8,9);
 
 }
 
