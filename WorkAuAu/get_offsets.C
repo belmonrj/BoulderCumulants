31271d992838d365128b2c3164d0e3cb284975d8
Ron Belmont <belmonrj@gmail.com>
2017-08-16 11:14:00 -0600

    Added north and south centrality offsets for recursion

--- WorkAuAu/get_offsets.C
+++ WorkAuAu/get_offsets.C
@@ -8,12 +8,16 @@ void get_offsets()
   // --- get the histograms (only looking at north and south for now, no higher detail)
   TProfile* nfvtxt_recoffsets[2][10] = {{NULL}};
   TProfile* centre_recoffsets[2][10] = {{NULL}};
+  TProfile* centre_recoffsets_north[2][10] = {{NULL}};
+  TProfile* centre_recoffsets_south[2][10] = {{NULL}};
   for ( int cs = 0; cs < 2; ++cs )
     {
       for ( int c = 0; c < 10; ++c )
         {
           nfvtxt_recoffsets[cs][c] = (TProfile*)fin->Get(Form("nfvtxt_recoffsets_%d_%d",cs,c));
           centre_recoffsets[cs][c] = (TProfile*)fin->Get(Form("nfvtxt_recoffsets_%d_%d",cs,c));
+          centre_recoffsets_north[cs][c] = (TProfile*)fin->Get(Form("nfvtxt_recoffsets_north_%d_%d",cs,c));
+          centre_recoffsets_south[cs][c] = (TProfile*)fin->Get(Form("nfvtxt_recoffsets_south_%d_%d",cs,c));
           // cout << nfvtxt_recoffsets[cs][c]->GetName() << " " << nfvtxt_recoffsets[cs][c] << endl;
           // cout << centre_recoffsets[cs][c]->GetName() << " " << centre_recoffsets[cs][c] << endl;
         }
@@ -53,6 +57,7 @@ void get_offsets()
     } // end of loop over number of tracks
   */
   // --- now loop over centralities
+  /*
   for ( int i = 0; i < 100; ++i ) // adjust as needed...
     {
       fout << "  ";
@@ -62,6 +67,43 @@ void get_offsets()
             {
               qvoff[cs][c] = centre_recoffsets[cs][c]->GetBinContent(i+1);
               sprintf(name,"qvoff_cent[%d][%d][%d]",i,cs,c);
+              //fout << setprecision(6);
+              char offset[10];
+              sprintf(offset,"%.6f",qvoff[cs][c]);
+              fout << name << " = " << offset << ";";
+              fout << "\t";
+            }
+        } // end
+      fout << endl;
+    } // end of loop over number of tracks
+  */
+  for ( int i = 0; i < 100; ++i ) // adjust as needed...
+    {
+      fout << "  ";
+      for ( int cs = 0; cs < 2; ++cs )
+        {
+          for ( int c = 0; c < 10; ++c )
+            {
+              qvoff[cs][c] = centre_recoffsets_north[cs][c]->GetBinContent(i+1);
+              sprintf(name,"qvoff_cent_north[%d][%d][%d]",i,cs,c);
+              //fout << setprecision(6);
+              char offset[10];
+              sprintf(offset,"%.6f",qvoff[cs][c]);
+              fout << name << " = " << offset << ";";
+              fout << "\t";
+            }
+        } // end
+      fout << endl;
+    } // end of loop over number of tracks
+  for ( int i = 0; i < 100; ++i ) // adjust as needed...
+    {
+      fout << "  ";
+      for ( int cs = 0; cs < 2; ++cs )
+        {
+          for ( int c = 0; c < 10; ++c )
+            {
+              qvoff[cs][c] = centre_recoffsets_south[cs][c]->GetBinContent(i+1);
+              sprintf(name,"qvoff_cent_south[%d][%d][%d]",i,cs,c);
               //fout << setprecision(6);
               char offset[10];
               sprintf(offset,"%.6f",qvoff[cs][c]);
