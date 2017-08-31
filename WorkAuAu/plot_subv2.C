4d2213a941d18a62d28182e294ffbaa54633282a
Ron Belmont <belmonrj@gmail.com>
2017-08-30 23:14:14 -0600

    Plotting essentials for preliminary

--- WorkAuAu/plot_subv2.C
+++ WorkAuAu/plot_subv2.C
@@ -101,41 +101,49 @@ void plot_subv2(TH1D* th1d_v24, TH1D* th1d_v24aabb, TH1D* th1d_v24abab, TH1D* th
   //if ( leg ) delete leg;
   TLegend* leg = new TLegend(0.62,0.72,0.88,0.92);
   //leg->SetHeader(type);
-  leg->SetHeader("Run14AuAu200");
-  leg->SetTextSize(0.045);
+  //leg->SetHeader("Run14AuAu200");
+  leg->SetTextSize(0.05);
   leg->SetFillStyle(0);
   leg->AddEntry(th1d_v24,"v_{2}{4}","p");
   leg->AddEntry(th1d_v24aabb,"v_{2}{4}_{aa|bb}","p");
   leg->AddEntry(th1d_v24abab,"v_{2}{4}_{ab|ab}","p");
   leg->Draw();
+  TLatex* tex_phenix = new TLatex(0.2,0.87,"PHENIX");
+  tex_phenix->SetTextSize(0.05);
+  tex_phenix->SetNDC();
+  tex_phenix->Draw();
+  TLatex* tex_system = new TLatex(0.2,0.80,"Au+Au #sqrt{s_{NN}} = 200 GeV");
+  tex_system->SetTextSize(0.05);
+  tex_system->SetNDC();
+  tex_system->Draw();
   c1->Print("FigsSubevents/cent_subevents4.png");
   c1->Print("FigsSubevents/cent_subevents4.pdf");
-  if ( iscent )
-  {
-  gv22_sys->GetXaxis()->SetRangeUser(1,90);
-  gv22ab_sys->GetXaxis()->SetRangeUser(1,90);
-  th1d_v22->GetXaxis()->SetRangeUser(1,90);
-  th1d_v22ab->GetXaxis()->SetRangeUser(1,90);
-  }
-  th1d_v22->SetMarkerStyle(kOpenCross);
-  th1d_v22->SetMarkerColor(kRed);
-  th1d_v22->SetLineColor(kRed);
-  th1d_v22->Draw("same ex0p");
-  gv22_sys->Draw("same E5");
-  th1d_v22ab->SetMarkerStyle(kOpenDiamond);
-  th1d_v22ab->SetMarkerColor(kMagenta+2);
-  th1d_v22ab->SetLineColor(kMagenta+2);
-  th1d_v22ab->Draw("same ex0p");
-  gv22ab_sys->Draw("same E5");
-  TLegend* leg2 = new TLegend(0.48,0.72,0.62,0.88);
-  //leg2->SetHeader("Run14AuAu200");
-  leg2->SetTextSize(0.045);
-  leg2->SetFillStyle(0);
-  leg2->AddEntry(th1d_v22,"v_{2}{2}","p");
-  leg2->AddEntry(th1d_v22ab,"v_{2}{2}_{a|b}","p");
-  leg2->Draw();
-  c1->Print("FigsSubevents/cent_subevents24.png");
-  c1->Print("FigsSubevents/cent_subevents24.pdf");
+  // if ( iscent )
+  // {
+  // gv22_sys->GetXaxis()->SetRangeUser(1,90);
+  // gv22ab_sys->GetXaxis()->SetRangeUser(1,90);
+  // th1d_v22->GetXaxis()->SetRangeUser(1,90);
+  // th1d_v22ab->GetXaxis()->SetRangeUser(1,90);
+  // }
+  // th1d_v22->SetMarkerStyle(kOpenCross);
+  // th1d_v22->SetMarkerColor(kRed);
+  // th1d_v22->SetLineColor(kRed);
+  // th1d_v22->Draw("same ex0p");
+  // gv22_sys->Draw("same E5");
+  // th1d_v22ab->SetMarkerStyle(kOpenDiamond);
+  // th1d_v22ab->SetMarkerColor(kMagenta+2);
+  // th1d_v22ab->SetLineColor(kMagenta+2);
+  // th1d_v22ab->Draw("same ex0p");
+  // gv22ab_sys->Draw("same E5");
+  // TLegend* leg2 = new TLegend(0.48,0.72,0.62,0.88);
+  // //leg2->SetHeader("Run14AuAu200");
+  // leg2->SetTextSize(0.045);
+  // leg2->SetFillStyle(0);
+  // leg2->AddEntry(th1d_v22,"v_{2}{2}","p");
+  // leg2->AddEntry(th1d_v22ab,"v_{2}{2}_{a|b}","p");
+  // leg2->Draw();
+  // c1->Print("FigsSubevents/cent_subevents24.png");
+  // c1->Print("FigsSubevents/cent_subevents24.pdf");
 
 
 }
