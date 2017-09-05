5ac98b1d2b0311b1e1c003c82d0106ffb08d1e3a
Ron Belmont <belmonrj@gmail.com>
2017-09-05 16:53:06 -0600

    Working on staging things for adding the preliminary logo

---
+++ WorkAuAu/copy_collect.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cp FigsRecursion/recursion_cent_v22.pdf CollectFigures
+cp FigsRecursion/recursion_cent_v224.pdf CollectFigures
+cp FigsRecursion/recursion_cent_v2246.pdf CollectFigures
+cp FigsRecursion/recursion_cent_v22468.pdf CollectFigures
+cp FigsRecursion/recursion_scent_v22.pdf CollectFigures
+cp FigsRecursion/recursion_scent_v224.pdf CollectFigures
+cp FigsRecursion/recursion_scent_v2246.pdf CollectFigures
+cp FigsRecursion/recursion_scent_v22468.pdf CollectFigures
+cp FigsSubevents/cent_subevents_v24x01.pdf CollectFigures
+cp FigsSubevents/cent_subevents_v24x02.pdf CollectFigures
+cp FigsSubevents/cent_subevents_v24x03.pdf CollectFigures
+cp FigsSigma/sigma_cent_x04.pdf CollectFigures
+cp FigsSigma/sigma_cent_x05.pdf CollectFigures
+cp FigsAmpt/comp_ampt_cumulant4.pdf CollectFigures
+cp FigsAmpt/sigma_ampt_x04.pdf CollectFigures
+cp FigsAmpt/sigma_ampt_x05.pdf CollectFigures
+
