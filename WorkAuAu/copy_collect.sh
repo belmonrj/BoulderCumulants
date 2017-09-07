44fab5de0353e83e1ba1f4ab7a2c943838435eb7
Ron Belmont <belmonrj@gmail.com>
2017-09-07 10:32:42 -0600

    Added special figs to logo stuff

--- WorkAuAu/copy_collect.sh
+++ WorkAuAu/copy_collect.sh
@@ -8,14 +8,24 @@ cp FigsRecursion/recursion_scent_v22.pdf CollectFigures
 cp FigsRecursion/recursion_scent_v224.pdf CollectFigures
 cp FigsRecursion/recursion_scent_v2246.pdf CollectFigures
 cp FigsRecursion/recursion_scent_v22468.pdf CollectFigures
+
 cp FigsSubevents/cent_subevents_v24x01.pdf CollectFigures
 cp FigsSubevents/cent_subevents_v24x02.pdf CollectFigures
 cp FigsSubevents/cent_subevents_v24x03.pdf CollectFigures
+
 cp FigsSigma/sigma_cent_x04.pdf CollectFigures
 cp FigsSigma/sigma_cent_x05.pdf CollectFigures
+
 cp FigsAmpt/comp_ampt_cumulant4.pdf CollectFigures
 cp FigsAmpt/sigma_ampt_x04.pdf CollectFigures
 cp FigsAmpt/sigma_ampt_x05.pdf CollectFigures
+
 cp FigsStar/star1_v224_Run14AuAu200.pdf CollectFigures
 cp FigsStar/starstar1_v224_Run14AuAu200.pdf CollectFigures
+
+cp FigsSpecial/special_cent_v22.pdf CollectFigures
+cp FigsSpecial/special_cent_v222.pdf CollectFigures
+cp FigsSpecial/special_cent_v2224.pdf CollectFigures
+cp FigsSpecial/special_cent_v22246.pdf CollectFigures
+cp FigsSpecial/special_cent_v222468.pdf CollectFigures
 
