84e93b9f2ded596806d98cd614d0c16c5ca0e594
Ron Belmont <belmonrj@gmail.com>
2017-10-02 19:40:51 -0600

    Modified SC code to to accept argumenent for cent and ntrk

--- WorkAuAu/simple_SC.C
+++ WorkAuAu/simple_SC.C
@@ -1,61 +1,103 @@
+void doit(TProfile*, TProfile*, TProfile*, TProfile*, TProfile*, int, const char*);
+
+
+
 void simple_SC()
 {
 
+  TFile* fin = TFile::Open("input/cumulants_Run14AuAu200.root");
+  TProfile* tp1f_2323 = (TProfile*)fin->Get("centrality_recursion_0_10");
+  TProfile* tp1f_2424 = (TProfile*)fin->Get("centrality_recursion_0_11");
+  TProfile* tp1f_22 = (TProfile*)fin->Get("centrality_recursion_0_0");
+  TProfile* tp1f_33 = (TProfile*)fin->Get("centrality_recursion_0_1");
+  TProfile* tp1f_44 = (TProfile*)fin->Get("centrality_recursion_0_7");
+
+  doit(tp1f_2323, tp1f_2424, tp1f_22, tp1f_33, tp1f_44, 1, "cent");
+
+  tp1f_2323 = (TProfile*)fin->Get("nfvtxt_recursion_0_10");
+  tp1f_2424 = (TProfile*)fin->Get("nfvtxt_recursion_0_11");
+  tp1f_22 = (TProfile*)fin->Get("nfvtxt_recursion_0_0");
+  tp1f_33 = (TProfile*)fin->Get("nfvtxt_recursion_0_1");
+  tp1f_44 = (TProfile*)fin->Get("nfvtxt_recursion_0_7");
+
+  doit(tp1f_2323, tp1f_2424, tp1f_22, tp1f_33, tp1f_44, 10, "ntrk");
+
+}
+
+
+
+void doit(TProfile* tp1f_2323, TProfile* tp1f_2424, TProfile* tp1f_22, TProfile* tp1f_33, TProfile* tp1f_44, int rebin, const char* handle)
+{
+
+  bool iscent = false;
+  bool isscent = false;
+  bool isntrk = false;
+  if ( strcmp(handle,"cent") == 0 ) iscent = true;
+  if ( strcmp(handle,"scent") == 0 ) {iscent = true; isscent = true;}
+  if ( strcmp(handle,"ntrk") == 0 ) isntrk = true;
+  bool isamptntrk = false;
+  if ( strcmp(handle,"amptntrk") == 0 ) { isamptntrk = true; isntrk = true; }
+  bool islowzoom = false;
+  if ( strcmp(handle,"lowntrkzoom") == 0 ) { islowzoom = true; isntrk = true; }
+
+  tp1f_2323->Rebin(rebin);
+  tp1f_2424->Rebin(rebin);
+  tp1f_22->Rebin(rebin);
+  tp1f_33->Rebin(rebin);
+  tp1f_44->Rebin(rebin);
+
+
+
   TCanvas* c1 = new TCanvas("c1","");
 
-  TFile* fin = TFile::Open("input/cumulants_Run14AuAu200.root");
-  TProfile* tp1f_cent_2323 = (TProfile*)fin->Get("centrality_recursion_0_10");
-  TProfile* tp1f_cent_2424 = (TProfile*)fin->Get("centrality_recursion_0_11");
-  TProfile* tp1f_cent_22 = (TProfile*)fin->Get("centrality_recursion_0_0");
-  TProfile* tp1f_cent_33 = (TProfile*)fin->Get("centrality_recursion_0_1");
-  TProfile* tp1f_cent_44 = (TProfile*)fin->Get("centrality_recursion_0_7");
+  tp1f_2323->Draw();
+  c1->Print(Form("FigsSymmetricCumulants/diagnostic_%s_2323.png",handle));
+  c1->Print(Form("FigsSymmetricCumulants/diagnostic_%s_2323.pdf",handle));
 
-  tp1f_cent_2323->Draw();
-  c1->Print("FigsSymmetricCumulants/diagnostic_cent_2323.png");
-  c1->Print("FigsSymmetricCumulants/diagnostic_cent_2323.pdf");
+  tp1f_2424->Draw();
+  c1->Print(Form("FigsSymmetricCumulants/diagnostic_%s_2424.png",handle));
+  c1->Print(Form("FigsSymmetricCumulants/diagnostic_%s_2424.pdf",handle));
 
-  tp1f_cent_2424->Draw();
-  c1->Print("FigsSymmetricCumulants/diagnostic_cent_2424.png");
-  c1->Print("FigsSymmetricCumulants/diagnostic_cent_2424.pdf");
+  tp1f_22->Draw();
+  c1->Print(Form("FigsSymmetricCumulants/diagnostic_%s_22.png",handle));
+  c1->Print(Form("FigsSymmetricCumulants/diagnostic_%s_22.pdf",handle));
 
-  tp1f_cent_22->Draw();
-  c1->Print("FigsSymmetricCumulants/diagnostic_cent_22.png");
-  c1->Print("FigsSymmetricCumulants/diagnostic_cent_22.pdf");
+  tp1f_33->Draw();
+  c1->Print(Form("FigsSymmetricCumulants/diagnostic_%s_33.png",handle));
+  c1->Print(Form("FigsSymmetricCumulants/diagnostic_%s_33.pdf",handle));
 
-  tp1f_cent_33->Draw();
-  c1->Print("FigsSymmetricCumulants/diagnostic_cent_33.png");
-  c1->Print("FigsSymmetricCumulants/diagnostic_cent_33.pdf");
+  tp1f_44->Draw();
+  c1->Print(Form("FigsSymmetricCumulants/diagnostic_%s_44.png",handle));
+  c1->Print(Form("FigsSymmetricCumulants/diagnostic_%s_44.pdf",handle));
 
-  tp1f_cent_44->Draw();
-  c1->Print("FigsSymmetricCumulants/diagnostic_cent_44.png");
-  c1->Print("FigsSymmetricCumulants/diagnostic_cent_44.pdf");
+  TH1D* th1d_2323 = (TH1D*)tp1f_2323->ProjectionX(Form("th1d_2323_%s",handle));
+  TH1D* th1d_2424 = (TH1D*)tp1f_2424->ProjectionX(Form("th1d_2424_%s",handle));
+  TH1D* th1d_22 = (TH1D*)tp1f_22->ProjectionX(Form("th1d_22_%s",handle));
+  TH1D* th1d_33 = (TH1D*)tp1f_33->ProjectionX(Form("th1d_33_%s",handle));
+  TH1D* th1d_44 = (TH1D*)tp1f_44->ProjectionX(Form("th1d_44_%s",handle));
 
-  TH1D* th1d_cent_2323 = (TH1D*)tp1f_cent_2323->ProjectionX("th1d_cent_2323");
-  TH1D* th1d_cent_2424 = (TH1D*)tp1f_cent_2424->ProjectionX("th1d_cent_2424");
-  TH1D* th1d_cent_22 = (TH1D*)tp1f_cent_22->ProjectionX("th1d_cent_22");
-  TH1D* th1d_cent_33 = (TH1D*)tp1f_cent_33->ProjectionX("th1d_cent_33");
-  TH1D* th1d_cent_44 = (TH1D*)tp1f_cent_44->ProjectionX("th1d_cent_44");
+  TH1D* th1d_2233 = (TH1D*)th1d_22->Clone();
+  th1d_2233->Multiply(th1d_33);
+  TH1D* th1d_SC23 = (TH1D*)th1d_2323->Clone();
+  th1d_SC23->Add(th1d_2233,-1.0);
 
-  TH1D* th1d_cent_2233 = (TH1D*)th1d_cent_22->Clone();
-  th1d_cent_2233->Multiply(th1d_cent_33);
-  TH1D* th1d_cent_SC23 = (TH1D*)th1d_cent_2323->Clone();
-  th1d_cent_SC23->Add(th1d_cent_2233,-1.0);
-
-  TH1D* th1d_cent_2244 = (TH1D*)th1d_cent_22->Clone();
-  th1d_cent_2244->Multiply(th1d_cent_44);
-  TH1D* th1d_cent_SC24 = (TH1D*)th1d_cent_2424->Clone();
-  th1d_cent_SC24->Add(th1d_cent_2244,-1.0);
+  TH1D* th1d_2244 = (TH1D*)th1d_22->Clone();
+  th1d_2244->Multiply(th1d_44);
+  TH1D* th1d_SC24 = (TH1D*)th1d_2424->Clone();
+  th1d_SC24->Add(th1d_2244,-1.0);
 
   // --- considering passing these as arguments...
   double xmin = 0.0;
   double xmax = 100.0;
   double ymin = -1e-5;
   double ymax = 1e-5;
+  if ( isntrk && !islowzoom ) xmax = 650.0;
 
   // --- do the plotting
   TH2D* empty = new TH2D("empty","",1,xmin,xmax,1,ymin,ymax);
   empty->Draw();
-  empty->GetXaxis()->SetTitle("Centrality (%)");
+  if ( iscent ) empty->GetXaxis()->SetTitle("Centrality (%)");
+  if ( isntrk ) empty->GetXaxis()->SetTitle("N_{tracks}^{FVTX}");
   empty->GetYaxis()->SetTitle("Symmetric Cumulant");
   empty->GetYaxis()->SetTitleOffset(1.2);
   TLatex* tex_tracks = new TLatex(0.2,0.83,"h^{#pm} 1<|#eta|<3");
@@ -67,20 +109,25 @@ void simple_SC()
   tex_system->SetNDC();
   tex_system->Draw();
   // ---
-  th1d_cent_SC23->SetMarkerStyle(kFullCircle);
-  th1d_cent_SC23->SetMarkerColor(kBlue);
-  th1d_cent_SC23->SetLineColor(kBlue);
-  th1d_cent_SC23->Draw("ex0p same");
-  th1d_cent_SC24->SetMarkerStyle(kFullSquare);
-  th1d_cent_SC24->SetMarkerColor(kRed);
-  th1d_cent_SC24->SetLineColor(kRed);
-  th1d_cent_SC24->Draw("ex0p same");
+  th1d_SC23->SetMarkerStyle(kFullCircle);
+  th1d_SC23->SetMarkerColor(kBlue);
+  th1d_SC23->SetLineColor(kBlue);
+  th1d_SC23->Draw("ex0p same");
+  th1d_SC24->SetMarkerStyle(kFullSquare);
+  th1d_SC24->SetMarkerColor(kRed);
+  th1d_SC24->SetLineColor(kRed);
+  th1d_SC24->Draw("ex0p same");
   TLegend* leg = new TLegend(0.6,0.6,0.8,0.8);
-  leg->AddEntry(th1d_cent_SC23,"SC(2,3)","p");
-  leg->AddEntry(th1d_cent_SC24,"SC(2,4)","p");
+  leg->AddEntry(th1d_SC23,"SC(2,3)","p");
+  leg->AddEntry(th1d_SC24,"SC(2,4)","p");
   leg->SetFillStyle(0);
   leg->SetTextSize(0.05);
   leg->Draw();
-  c1->Print("FigsSymmetricCumulants/physics_cent.png");
+  c1->Print(Form("FigsSymmetricCumulants/physics_%s.png",handle));
+  c1->Print(Form("FigsSymmetricCumulants/physics_%s.pdf",handle));
+
+  delete c1;
+  delete empty;
+  delete leg;
 
 }
