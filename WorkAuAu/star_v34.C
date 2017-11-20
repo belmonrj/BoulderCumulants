61f27e50f74205d2f0396d8357b2c563f3d91efa
Ron Belmont <belmonrj@gmail.com>
2017-11-20 15:39:58 -0700

    Added v342 and ratio plot, ratio plot not so useful till some isses with central v32 are better understood

--- WorkAuAu/star_v34.C
+++ WorkAuAu/star_v34.C
@@ -1,6 +1,7 @@
 int rebin = 5;
 
 TH1D* get_cumuhist(TFile*);
+TH1D* get_histv324(TFile*);
 
 void star_v34()
 {
@@ -8,6 +9,7 @@ void star_v34()
   TFile* fileR = TFile::Open("RunByRun/combin.root"); // reference
 
   TH1D* histR = get_cumuhist(fileR); // reference
+  TH1D* histS = get_histv324(fileR); // reference
 
   const int nbins = histR->GetNbinsX();
 
@@ -20,6 +22,12 @@ void star_v34()
   double norm_ev3[nbins];
   double swap_ev3[nbins];
   double cent[nbins];
+  double norm_c2[nbins];
+  double swap_c2[nbins];
+  double norm_ec2[nbins];
+  double swap_ec2[nbins];
+  double ratio[nbins];
+  double eratio[nbins];
   for ( int i = 0; i < nbins; ++i )
     {
       cent[i] = histR->GetBinCenter(i+1);
@@ -37,21 +45,41 @@ void star_v34()
       swap_ev3[i] = (1/(4*pow(-swap_c3[i],0.75)))*swap_ec3[i];
       if ( swap_v3[i] != swap_v3[i] ) swap_v3[i] = -999;
       if ( swap_ev3[i] != swap_ev3[i] ) swap_v3[i] = 998;
+      // --- v3{2}^4
+      swap_c2[i] = histS->GetBinContent(i+1);
+      swap_ec2[i] = histS->GetBinError(i+1);
+      ratio[i] = 2 - swap_c3[i]/swap_c2[i];
+      // --- way too big
+      eratio[i] = ratio[i]*sqrt( pow( (swap_ec3[i]/swap_c3[i]), 2.0 ) + pow( (swap_ec2[i]/swap_c2[i]), 2.0 ) );
+      eratio[i] = (swap_ec2[i]/swap_c2[i]) * 50;
       swap_c3[i] *= 1e6;
       swap_ec3[i] *= 1e6;
-      cout << cent[i] << " " << swap_c3[i] << endl;
+      swap_c2[i] *= 1e6;
+      swap_ec2[i] *= 1e6;
+      // --- ratio
+      // cout << cent[i] << " "
+      //      << swap_c3[i] << " "
+      //      << swap_c2[i] << " "
+      //      << ratio[i] << " +- "
+      //      << eratio[i]
+      //      << endl;
+      cout << cent[i] << " "
+           << swap_ec3[i]/swap_c3[i] << " "
+           << swap_ec2[i]/swap_c2[i] << " "
+           << eratio[i]/ratio[i] << " +- "
+           << endl;
     }
 
 
 
-// 106*v34{4}	2 - (v34{4}/v34{2})
-// cent (%)	STAR	error	ALICE	error	STAR	error	ALICE	error
-// 2.5	-0.00156	0.004	0.0123	0.0047	2.04	0.096	1.95	0.021
-// 7.5	0.00549	        0.0058	0.025	0.0092	1.92	0.082	1.94	0.022
-// 15	0.000938	0.0069	0.0581	0.01	1.99	0.063	1.91	0.015
-// 25	0.00728	        0.012	0.0514	0.02	1.95	0.087	1.95	0.018
-// 35	-0.0346	        0.026	0.114	0.037	2.21	0.15	1.92	0.024
-// 45	-0.0362	        0.057	0.0983	0.078	2.21	0.34	1.95	0.04
+  // 106*v34{4}	2 - (v34{4}/v34{2})
+  // cent (%)	STAR	        error	ALICE	error	STAR	error	ALICE	error
+  // 2.5	-0.00156	0.004	0.0123	0.0047	2.04	0.096	1.95	0.021
+  // 7.5	0.00549	        0.0058	0.025	0.0092	1.92	0.082	1.94	0.022
+  // 15	        0.000938	0.0069	0.0581	0.01	1.99	0.063	1.91	0.015
+  // 25	        0.00728	        0.012	0.0514	0.02	1.95	0.087	1.95	0.018
+  // 35	        -0.0346	        0.026	0.114	0.037	2.21	0.15	1.92	0.024
+  // 45	        -0.0362	        0.057	0.0983	0.078	2.21	0.34 	1.95	0.04
 
   float star_cent[6] = {2.5,7.5,15,25,35,45};
   float star_v34[6] = {-0.00156,
@@ -66,10 +94,25 @@ void star_v34()
                         0.012,
                         0.026,
                         0.057};
+  float star_ratio[6] = {2.04,
+                         1.92,
+                         1.99,
+                         1.95,
+                         2.21,
+                         2.21};
+  float star_eratio[6] = {0.096,
+                          0.082,
+                          0.063,
+                          0.087,
+                          0.15,
+                          0.34};
 
   //TGraphErrors* tge_norm = new TGraphErrors(nbins,cent,norm_v3,0,norm_ev3);
   TGraphErrors* tge_norm = new TGraphErrors(6,star_cent,star_v34,0,star_ev34);
   TGraphErrors* tge_swap = new TGraphErrors(nbins,cent,swap_c3,0,swap_ec3);
+  TGraphErrors* tge_swap2 = new TGraphErrors(nbins,cent,swap_c2,0,swap_ec2);
+  TGraphErrors* tge_ratio = new TGraphErrors(nbins,cent,ratio,0,eratio);
+  TGraphErrors* tge_sratio = new TGraphErrors(6,star_cent,star_ratio,0,star_eratio);
 
 
 
@@ -81,6 +124,15 @@ void star_v34()
   tge_swap->SetLineColor(kBlack);
   tge_swap->SetMarkerColor(kBlack);
   tge_swap->SetMarkerStyle(kOpenCircle);
+  tge_swap2->SetLineColor(kBlack);
+  tge_swap2->SetMarkerColor(kBlack);
+  tge_swap2->SetMarkerStyle(kFullCircle);
+  tge_ratio->SetLineColor(kBlack);
+  tge_ratio->SetMarkerColor(kBlack);
+  tge_ratio->SetMarkerStyle(kOpenCircle);
+  tge_sratio->SetLineColor(kBlack);
+  tge_sratio->SetMarkerColor(kBlack);
+  tge_sratio->SetMarkerStyle(kFullCircle);
 
   TCanvas* c1 = new TCanvas("c1","");
 
@@ -109,6 +161,58 @@ void star_v34()
   c1->Print("v34_star.png");
 
 
+  ymin = -0.2;
+  ymax = 0.5;
+  delete hdummy;
+  hdummy = new TH2D("hdummy","",1,xmin,xmax,1,ymin,ymax);
+  hdummy->Draw();
+  hdummy->GetYaxis()->SetTitle(Form("10^{6} v_{3}^{4}"));
+  hdummy->GetYaxis()->SetTitleOffset(1.25);
+  hdummy->GetXaxis()->SetTitle("Centrality (%)");
+  tge_swap->Draw("pz");
+  tge_swap2->Draw("pz");
+  delete leg;
+  leg = new TLegend(0.18,0.68,0.38,0.88);
+  leg->SetFillStyle(0);
+  //leg->SetHeader("10^{6} v_{3}^{4}");
+  leg->AddEntry(tge_swap,"PHENIX v_{3}^{4}{4}","p");
+  leg->AddEntry(tge_swap2,"PHENIX v_{3}^{4}{2}","p");
+  leg->SetTextSize(0.05);
+  leg->Draw();
+  delete line;
+  line = new TLine(xmin,0,xmax,0);
+  line->SetLineStyle(2);
+  line->SetLineWidth(2);
+  line->Draw();
+  c1->Print("v342_star.png");
+
+
+  ymin = 0.0;
+  ymax = 5.0;
+  delete hdummy;
+  hdummy = new TH2D("hdummy","",1,xmin,xmax,1,ymin,ymax);
+  hdummy->Draw();
+  hdummy->GetYaxis()->SetTitle(Form("2 - v_{3}^{4}{4}/v_{3}^{4}{2}"));
+  hdummy->GetYaxis()->SetTitleOffset(1.25);
+  hdummy->GetXaxis()->SetTitle("Centrality (%)");
+  tge_ratio->Draw("pz");
+  tge_sratio->Draw("pz");
+  delete leg;
+  leg = new TLegend(0.18,0.18,0.38,0.38);
+  leg->SetFillStyle(0);
+  //leg->SetHeader("10^{6} v_{3}^{4}");
+  leg->AddEntry(tge_ratio,"PHENIX","p");
+  leg->AddEntry(tge_sratio,"STAR, Phys. Rev. C 88 (2013) 14904","p");
+  leg->SetTextSize(0.05);
+  leg->Draw();
+  delete line;
+  line = new TLine(xmin,2,xmax,2);
+  line->SetLineStyle(2);
+  line->SetLineWidth(2);
+  line->Draw();
+  c1->Print("v342_ratio_star.png");
+
+
 }
 
 TH1D* get_cumuhist(TFile* fin)
@@ -134,3 +238,21 @@ TH1D* get_cumuhist(TFile* fin)
   // --- return the cumulant
   return th1d_cu4;
 }
+
+TH1D* get_histv324(TFile* fin)
+{
+  // --- random number helper to prevent memory collisions with ROOT named objects...
+  double rand = gRandom->Rndm();
+  int helper = rand*10000;
+  // --- get the tprofiles
+  TProfile* ctp1f_two = (TProfile*)fin->Get("centrality_recursion_0_1");
+  //ctp1f_two->Rebin(rebin); // maybe already rebinned above? need to add clones to prevent this
+  // --- convert to th1ds (to do math operations)
+  TH1D* th1d_two = ctp1f_two->ProjectionX(Form("th1d_two_%d",helper)); // <2>
+  // --- calc 22
+  TH1D* th1d_22 = (TH1D*)th1d_two->Clone(Form("th1d_222_%d",helper)); // <2>^2
+  th1d_22->Multiply(th1d_two);
+  // --- return
+  return th1d_22;
+}
+
