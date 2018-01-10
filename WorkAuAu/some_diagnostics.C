29f936cfddb109261fe74d657dbdfad6f708cad5
Ron Belmont <belmonrj@gmail.com>
2018-01-10 11:19:46 -0700

    Added rebinned coordinate histos

--- WorkAuAu/some_diagnostics.C
+++ WorkAuAu/some_diagnostics.C
@@ -1,10 +1,12 @@
 void track_parameters();
 void track_coordinates();
+void track_coordinates_rebinned();
 
 void some_diagnostics()
 {
   //track_parameters();
-  track_coordinates();
+  //track_coordinates();
+  track_coordinates_rebinned();
 }
 
 void track_parameters()
@@ -156,4 +158,99 @@ void track_coordinates()
     }
 
 } // track_coordinates
+
+void track_coordinates_rebinned()
+{
+
+  int rebin = 5;
+
+  TCanvas* c1 = new TCanvas("c1","");
+
+  TFile* file = TFile::Open("input/histos_12587.root");
+
+  // --- before_phi (phi distribution before track selection cuts)
+  TH2D* th2d_before_phi = (TH2D*)file->Get("th2d_track_before_phi");
+  TH1D* th1d_before_phi = th2d_before_phi->ProjectionY("th1d_before_phi",1,100);
+  th1d_before_phi->Rebin(rebin);
+  th1d_before_phi->Draw();
+  c1->Print("FigsDiagnostics/th1d_before_phi_R.png");
+  for ( int i = 0; i < 100; ++i )
+    {
+      TH1D* th1d_cent_before_phi = th2d_before_phi->ProjectionY(Form("th1d_cent%02d_before_phi",i),i+1,i+1);
+      th1d_cent_before_phi->Rebin(rebin);
+      th1d_cent_before_phi->Draw();
+      c1->Print(Form("FigsDiagnostics/Centrality/th1d_cent%02d_before_phi_R.png",i));
+    }
+
+  // --- after_phi (phi distribution after track selection cuts)
+  TH2D* th2d_after_phi = (TH2D*)file->Get("th2d_track_after_phi");
+  TH1D* th1d_after_phi = th2d_after_phi->ProjectionY("th1d_after_phi",1,100);
+  th1d_after_phi->Rebin(rebin);
+  th1d_after_phi->Draw();
+  c1->Print("FigsDiagnostics/th1d_after_phi_R.png");
+  for ( int i = 0; i < 100; ++i )
+    {
+      TH1D* th1d_cent_after_phi = th2d_after_phi->ProjectionY(Form("th1d_cent%02d_after_phi",i),i+1,i+1);
+      th1d_cent_after_phi->Rebin(rebin);
+      th1d_cent_after_phi->Draw();
+      c1->Print(Form("FigsDiagnostics/Centrality/th1d_cent%02d_after_phi_R.png",i));
+    }
+
+  // --- aafter_phi (phi distribution after track selection cuts AND double track cut)
+  TH2D* th2d_aafter_phi = (TH2D*)file->Get("th2d_track_aafter_phi");
+  TH1D* th1d_aafter_phi = th2d_aafter_phi->ProjectionY("th1d_aafter_phi",1,100);
+  th1d_aafter_phi->Rebin(rebin);
+  th1d_aafter_phi->Draw();
+  c1->Print("FigsDiagnostics/th1d_aafter_phi_R.png");
+  for ( int i = 0; i < 100; ++i )
+    {
+      TH1D* th1d_cent_aafter_phi = th2d_aafter_phi->ProjectionY(Form("th1d_cent%02d_aafter_phi",i),i+1,i+1);
+      th1d_cent_aafter_phi->Rebin(rebin);
+      th1d_cent_aafter_phi->Draw();
+      c1->Print(Form("FigsDiagnostics/Centrality/th1d_cent%02d_aafter_phi_R.png",i));
+    }
+
+  // --- before_eta (eta distribution before track selection cuts)
+  TH2D* th2d_before_eta = (TH2D*)file->Get("th2d_track_before_eta");
+  TH1D* th1d_before_eta = th2d_before_eta->ProjectionY("th1d_before_eta",1,100);
+  th1d_before_eta->Rebin(rebin);
+  th1d_before_eta->Draw();
+  c1->Print("FigsDiagnostics/th1d_before_eta_R.png");
+  for ( int i = 0; i < 100; ++i )
+    {
+      TH1D* th1d_cent_before_eta = th2d_before_eta->ProjectionY(Form("th1d_cent%02d_before_eta",i),i+1,i+1);
+      th1d_cent_before_eta->Rebin(rebin);
+      th1d_cent_before_eta->Draw();
+      c1->Print(Form("FigsDiagnostics/Centrality/th1d_cent%02d_before_eta_R.png",i));
+    }
+
+  // --- after_eta (eta distribution after track selection cuts)
+  TH2D* th2d_after_eta = (TH2D*)file->Get("th2d_track_after_eta");
+  TH1D* th1d_after_eta = th2d_after_eta->ProjectionY("th1d_after_eta",1,100);
+  th1d_after_eta->Rebin(rebin);
+  th1d_after_eta->Draw();
+  c1->Print("FigsDiagnostics/th1d_after_eta_R.png");
+  for ( int i = 0; i < 100; ++i )
+    {
+      TH1D* th1d_cent_after_eta = th2d_after_eta->ProjectionY(Form("th1d_cent%02d_after_eta",i),i+1,i+1);
+      th1d_cent_after_eta->Rebin(rebin);
+      th1d_cent_after_eta->Draw();
+      c1->Print(Form("FigsDiagnostics/Centrality/th1d_cent%02d_after_eta_R.png",i));
+    }
+
+  // --- aafter_eta (eta distribution after track selection cuts AND double track cut)
+  TH2D* th2d_aafter_eta = (TH2D*)file->Get("th2d_track_aafter_eta");
+  TH1D* th1d_aafter_eta = th2d_aafter_eta->ProjectionY("th1d_aafter_eta",1,100);
+  th1d_aafter_eta->Rebin(rebin);
+  th1d_aafter_eta->Draw();
+  c1->Print("FigsDiagnostics/th1d_aafter_eta_R.png");
+  for ( int i = 0; i < 100; ++i )
+    {
+      TH1D* th1d_cent_aafter_eta = th2d_aafter_eta->ProjectionY(Form("th1d_cent%02d_aafter_eta",i),i+1,i+1);
+      th1d_cent_aafter_eta->Rebin(rebin);
+      th1d_cent_aafter_eta->Draw();
+      c1->Print(Form("FigsDiagnostics/Centrality/th1d_cent%02d_aafter_eta_R.png",i));
+    }
+
+} // track_coordinates_rebinned
 
