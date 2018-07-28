c6a83716de1302304d9547aadfc44866e1358427
Ron Belmont <belmonrj@gmail.com>
2018-07-28 17:52:17 -0400

    Removing hard coded constants in favor of locally stored histogram file

--- BoulderCumulantsOffsetsRBR.C
+++ BoulderCumulantsOffsetsRBR.C
@@ -6,6 +6,39 @@ using std::endl;
 
 void BoulderCumulants::SetQvectorOffsetsRBR(int run)
 {
-  cout << "Since the previous version took 20 minutes to compile, let's do nothing until we come up with a better way..." << endl;
-}
+
+  //cout << "Since the previous version took 20 minutes to compile, let's do nothing until we come up with a better way..." << endl;
+  if ( run < 0 ) return;
+  if ( _verbosity > 1 ) cout << "setting Q-vector offsets..." << endl;
+
+  TFile* file = TFile::Open("rbr_offsets.root");
+  if ( file == NULL )
+    {
+      cout << "Offset histogram file not found" << endl;
+      return; // should modify routine to add return values to check...
+    }
+
+  for ( int cs = 0; cs < 2; ++cs )
+    {
+      for ( int c = 0; c < maxHarmonic; ++c )
+        {
+          TProfile* tp1f_cent_north = (TProfile*)file->Get(Form("run%d_centrality_recoffsets_north_%d_%d",run,cs,c));
+          TProfile* tp1f_cent_south = (TProfile*)file->Get(Form("run%d_centrality_recoffsets_south_%d_%d",run,cs,c));
+          if ( tp1f_cent_north || tp1f_cent_south )
+            {
+              cout << "Offset histogram(s) not found" << endl;
+              cout << tp1f_cent_north << endl;
+              cout << tp1f_cent_south << endl;
+              return; // should modify routine to add return values to check...
+            }
+          for ( int i = 0; i < 100; ++i )
+            {
+              qvoff_cent_north[i][cs][c] = tp1f_cent_north->GetBinContent(i+1);
+              qvoff_cent_south[i][cs][c] = tp1f_cent_south->GetBinContent(i+1);
+            } // loop over centralities
+        } // loop over harmonics
+    } // loop over cos (0) or sin (1)
+
+} // void BoulderCumulants::SetQvectorOffsetsRBR(int run)
+
 
