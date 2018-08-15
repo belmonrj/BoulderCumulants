7509d6e3046c84a4deed80645117cb00d51db4b3
Ron Belmont <belmonrj@gmail.com>
2018-08-15 11:37:44 -0400

    Added third harmonic to subevent cumulant

--- WorkAuAu/get_subevents.C
+++ WorkAuAu/get_subevents.C
@@ -33,4 +33,31 @@ void get_subevents()
   plot_subv2(hv24, hv24aabb, hv24abab, hv22, hv22ab);
   plot_subc2(hc24, hc24aabb, hc24abab);
 
+  // --- now third harmonic
+
+  TProfile* tp1f_h3_for = (TProfile*)fin->Get("centrality_os_fvtxc_tracks_c34");
+  TProfile* tp1f_h3_4aabb = (TProfile*)fin->Get("centrality_os_fvtxsfvtxn_tracks_c34a");
+  TProfile* tp1f_h3_4abab = (TProfile*)fin->Get("centrality_os_fvtxsfvtxn_tracks_c34b");
+  TProfile* tp1f_h3_two = (TProfile*)fin->Get("centrality_os_fvtxc_tracks_c32");
+  TProfile* tp1f_h3_2aa = (TProfile*)fin->Get("centrality_os_fvtxs_tracks_c32");
+  TProfile* tp1f_h3_2bb = (TProfile*)fin->Get("centrality_os_fvtxn_tracks_c32");
+  TProfile* tp1f_h3_2ab = (TProfile*)fin->Get("centrality_os_fvtxsfvtxn_tracks_c32");
+  TH1D* hv34 = NULL;
+  TH1D* hv34aabb = NULL;
+  TH1D* hv34abab = NULL;
+  TH1D* hv32 = NULL;
+  TH1D* hv32ab = NULL;
+  TH1D* hc34 = NULL;
+  TH1D* hc34aabb = NULL;
+  TH1D* hc34abab = NULL;
+  TH1D* hc32 = NULL;
+  TH1D* hc32ab = NULL;
+  calc_subevents(tp1f_h3_for, tp1f_h3_4aabb, tp1f_h3_4abab,
+                 tp1f_h3_two, tp1f_h3_2aa, tp1f_h3_2bb, tp1f_h3_2ab,
+                 &hv34, &hv34aabb, &hv34abab, &hv32, &hv32ab,
+                 &hc34, &hc34aabb, &hc34abab, &hc32, &hc32ab,
+                 1);
+  //plot_subv2(hv34, hv34aabb, hv34abab, hv32, hv32ab);
+  plot_subc2(hc34, hc34aabb, hc34abab, 3);
+
 }
