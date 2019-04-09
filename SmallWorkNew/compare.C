67bbd4934459c6bfc9ce31f9f737a5db1219b7f7
Ron Belmont <belmonrj@gmail.com>
2019-04-09 12:24:16 -0400

    This comparison does not yield what I was expecting, but is certainly revelatory...

--- SmallWorkNew/compare.C
+++ SmallWorkNew/compare.C
@@ -7,8 +7,8 @@ void compare()
 
   // ---
 
-  //TFile* fin1 = TFile::Open("temp.root");
-  TFile* fin1 = TFile::Open("tree_temp.root");
+  TFile* fin1 = TFile::Open("temp.root");
+  //TFile* fin1 = TFile::Open("tree_temp.root");
   TFile* fin2 = TFile::Open("improved_20170517-2216.root"); // checking against an old file
 
   TProfile* tp1f_two1 = (TProfile*)fin1->Get("nfvtxt_ac_fvtxc_tracks_c22");
