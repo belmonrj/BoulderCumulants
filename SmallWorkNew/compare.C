45840f1bf31d53a975056b162228e2ad54d057bf
Ron Belmont <belmonrj@gmail.com>
2019-04-22 22:51:39 -0400

    Disappointing, but need to do a few more checks before trying other stuff

--- SmallWorkNew/compare.C
+++ SmallWorkNew/compare.C
@@ -9,7 +9,8 @@ void compare()
 
   //TFile* fin1 = TFile::Open("temp.root");
   //TFile* fin1 = TFile::Open("tree_temp.root");
-  TFile* fin1 = TFile::Open("tree_new.root");
+  //TFile* fin1 = TFile::Open("tree_new.root");
+  TFile* fin1 = TFile::Open("new_histos.root");
   TFile* fin2 = TFile::Open("improved_20170517-2216.root"); // checking against an old file
 
   TProfile* tp1f_two1 = (TProfile*)fin1->Get("nfvtxt_ac_fvtxc_tracks_c22");
