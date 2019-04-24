45531b4eb9edd395b97ae7da641222a86ab506ec
Ron Belmont <belmonrj@gmail.com>
2019-04-24 09:38:39 -0400

    Added new file for most recent taxi pass

--- SmallWorkNew/compare.C
+++ SmallWorkNew/compare.C
@@ -7,12 +7,12 @@ void compare()
 
   // ---
 
-  //TFile* fin1 = TFile::Open("temp.root");
-  //TFile* fin1 = TFile::Open("tree_temp.root");
-  //TFile* fin1 = TFile::Open("tree_new.root");
-  TFile* fin1 = TFile::Open("new_histos.root");
-  TFile* fin2 = TFile::Open("temp.root"); // checking against recent taxi pass
-  //TFile* fin2 = TFile::Open("improved_20170517-2216.root"); // checking against an old file
+  //TFile* fin1 = TFile::Open("tree_old.root"); // first of the recent tree passes
+  //TFile* fin1 = TFile::Open("tree_new.root"); // second of the recent tree passes
+  TFile* fin1 = TFile::Open("histos_14713.root"); // second of the recent taxi passes
+  //TFile* fin2 = TFile::Open("histos_14650.root"); // first of the recent taxi passes
+  //TFile* fin2 = TFile::Open("histos_14650.root"); // first of the recent taxi passes
+  TFile* fin2 = TFile::Open("improved_20170517-2216.root"); // an old file with results for PPG206
 
   TProfile* tp1f_two1 = (TProfile*)fin1->Get("nfvtxt_ac_fvtxc_tracks_c22");
   TProfile* tp1f_two2 = (TProfile*)fin2->Get("nfvtxt_ac_fvtxc_tracks_c22");
